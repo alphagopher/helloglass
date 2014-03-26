9d395ca2c4b0f91d605e91a6f2fcd4bf732117a4
Andrew Porter <porter.andrewsteven@gmail.com>
2014-03-25 22:05:00 -0500

    Removed unused icons

--- HelloGlass/gen/com/aporter/helloglass/R.java
+++ HelloGlass/gen/com/aporter/helloglass/R.java
@@ -18,24 +18,21 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-    }
-    public static final class drawable {
-        public static final int goldy=0x7f020000;
+        public static final int activity_horizontal_margin=0x7f030000;
+        public static final int activity_vertical_margin=0x7f030001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f070000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f060000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050004;
-        public static final int app_name=0x7f050000;
-        public static final int start_helloglass=0x7f050003;
-        public static final int text_footer=0x7f050002;
-        public static final int text_main=0x7f050001;
+        public static final int action_settings=0x7f040004;
+        public static final int app_name=0x7f040000;
+        public static final int start_helloglass=0x7f040003;
+        public static final int text_footer=0x7f040002;
+        public static final int text_main=0x7f040001;
     }
     public static final class style {
         /** 
@@ -59,16 +56,16 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. <style name="AppTheme" parent="AppBaseTheme"> 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f050001;
         /**  Stole this theme from the Google Stopwatch example in order to get the header to disappear on the Card 
          */
-        public static final int MenuTheme=0x7f060002;
+        public static final int MenuTheme=0x7f050002;
     }
     public static final class xml {
-        public static final int voice_trigger_start=0x7f030000;
+        public static final int voice_trigger_start=0x7f020000;
     }
 }
