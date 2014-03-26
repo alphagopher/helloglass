ae1ae179aad1089a147d39706d9de6442b02e80f
Andrew Porter <porter.andrewsteven@gmail.com>
2014-03-25 21:58:58 -0500

    Added error handling and logging features to MainActivity.java

--- HelloGlass/src/com/aporter/helloglass/MainActivity.java
+++ HelloGlass/src/com/aporter/helloglass/MainActivity.java
@@ -1,25 +1,34 @@
 package com.aporter.helloglass;
 
-//import android.accounts.Account;
-//import android.accounts.AccountManager;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.google.android.glass.app.Card;
 
 public class MainActivity extends Activity {
 
+	
+	private static final String TAG = "HelloGlass";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		//AccountManager mAccountManager = AccountManager.get(this);
-	    //Account[] accounts = mAccountManager.getAccountsByType(null);
-		//accounts.toString();
-		Card card = new Card(this);
-		card.setText(R.string.text_main);
-	    card.setFootnote(R.string.text_footer);
-	    //setContentView(R.layout.activity_main);
-		setContentView(card.toView());
+		try {
+			
+			Card card = new Card(this);
+			card.setText(R.string.text_main);
+		    card.setFootnote(R.string.text_footer);
+		    
+			setContentView(card.toView());
+
+		} catch (Exception e) {
+			
+			Log.d(TAG, e.getMessage());
+			System.exit(0);
+		}
+
 	}
 
 }
